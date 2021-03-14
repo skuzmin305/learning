273e39009a56090e775cd3170b0cfc7e228b4fd9
Sergey Kumzin <skuzmin305@gmail.com>
2021-03-14 07:57:40 +0300

    frdgstrg

--- quick_sort.c
+++ quick_sort.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+void quick_sort(data)
+{
+	int size = (sizeof(data) / sizeof(int)) / 3;
+	int less_then[size];
+	int equal[size];
+	int more_than[size];
+}
 int main(void)
 {
+	int my_list[] = {5, 12, 4, 32, 188, 16, 78, 1, 41, 75};
 	return 0;
 }
